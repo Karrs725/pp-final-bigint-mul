93460f254bedabd32ba5f3a1395f08abad4d0ec4
Karrs725 <karrs600914@gmail.com>
2025-11-27 01:47:58 +0800

    add test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,25 +1,25 @@
 set -e
 
-# for ((i=0;i<3;i=i+1)) do
-#     python3 generate.py > input
-#     python3 ans.py < input > ans
-#     time ./$1 input > output
-#     diff output ans || break
-# done
-# rm input output ans
-# echo "[PASSED]"
+# cmake -B build && cmake --build build -j
 
-
-python3 generate.py
-python3 ans.py
+rm *.out || true
+for ((d=3;d<=5;d=d+1)) do
+    for ((i=1;i<=5;i=i+1)) do
+        echo "Processing nd${d}_${i}.in"
+        ../build/bin/benchmark --impl long-cpu --file ../benchmarks/random/nd${d}_${i}.in >> long.out
+        ../build/bin/benchmark --impl long-cpu-simd --file ../benchmarks/random/nd${d}_${i}.in >> long-simd.out
+        ../build/bin/benchmark --impl long-cpu-thread --file ../benchmarks/random/nd${d}_${i}.in >> long-thread.out
+        ../build/bin/benchmark --impl long-cpu-thread-simd --file ../benchmarks/random/nd${d}_${i}.in >> long-thread-simd.out
+        ../build/bin/benchmark --impl long-cpu-omp --file ../benchmarks/random/nd${d}_${i}.in >> long-omp.out
+    done
+done
 
 for ((d=3;d<=6;d=d+1)) do
     for ((i=1;i<=5;i=i+1)) do
-        echo "Testing nd${d}_${i}..."
-        time ./$1 ../benchmarks/random/nd${d}_${i}.in $2 > output
-        diff output ../benchmarks/random/nd${d}_${i}.ans || ((echo "xxx") && exit 1)
-        echo "[PASSED]"
-        echo ""
+        echo "Processing nd${d}_${i}.in"
+        ../build/bin/benchmark --impl ntt-cpu --file ../benchmarks/random/nd${d}_${i}.in >> ntt.out
+        ../build/bin/benchmark --impl ntt-cpu-thread --file ../benchmarks/random/nd${d}_${i}.in >> ntt-thread.out
+        ../build/bin/benchmark --impl ntt-cpu-omp --file ../benchmarks/random/nd${d}_${i}.in >> ntt-omp.out
+        ../build/bin/benchmark --impl ntt-cpu-thread-old --file ../benchmarks/random/nd${d}_${i}.in >> ntt-thread-old.out
     done
 done
-rm output
